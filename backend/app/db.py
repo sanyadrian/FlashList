0da43188490e9084081b8173d84923439c07d4d0
sanyadrian <adrianovoleksandr@gmail.com>
2025-06-01 21:03:20 -0400

    Updated the db

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,8 +1,11 @@
 from sqlmodel import SQLModel, create_engine, Session
+import os
 
-DATABASE_URL = "sqlite:///flashlist.db"
+# DATABASE_URL = "sqlite:///flashlist.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, echo=False)
+# engine = create_engine(DATABASE_URL, echo=False)
+engine = create_engine(DATABASE_URL, echo=False, connect_args={})
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
